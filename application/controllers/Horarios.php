5963c8ef1a3db37bc87b50fe85cabc309041aafb
willian0795 <sonryjr@gmail.com>
2017-10-04 12:58:14 -0600

    agregando

--- application/controllers/Horarios.php
+++ application/controllers/Horarios.php
@@ -5,14 +5,58 @@ class Horarios extends CI_Controller {
 
 	function __construct(){
 		parent::__construct();
+		/************ Librerias para llamar funciones predefenidas **********/
 		$this->load->helper('url');
+		$this->load->helper('form');
+		$this->load->helper('funciones_rapidas');
+		$this->load->model('horarios_model');
 	}
 
-	public function index()
-	{
+	public function index(){
+		$var["horarios"] = $this->horarios_model->mostrar_horario();
+		$var["notificacion"] = "nada";
+		$this->vista_horario($var);
+	}
+
+	public function vista_horario($var){
 		$this->load->view('templates/header');
-		$this->load->view('configuraciones/horarios');
+		$this->load->view('configuraciones/horarios',$var);
 		$this->load->view('templates/footer');
+	}
+
+	public function gestionar_horarios(){
+		/************ Notificación a mostrar *****************/
+		$var["notificacion"] = "nada";
+
+		if($this->input->post('band') == "save"){
+			$data = array(
+			'descripcion' => $this->input->post('descripcion'), 
+			'hora_inicio' => date("Y-m-d ").$this->input->post('hora_inicio'),
+			'hora_fin' => date("Y-m-d ").$this->input->post('hora_fin')
+			);
+			$this->horarios_model->insertar_horario($data);
+			$var["notificacion"] = "<strong>Éxito!</strong> Viático: '".$this->input->post('descripcion')."' registrado exitosamente.";
+		}else if($this->input->post('band') == "edit"){
+			$data = array(
+			'idhorario' => $this->input->post('idhorario'), 
+			'descripcion' => $this->input->post('descripcion'), 
+			'hora_inicio' => date("Y-m-d ").$this->input->post('hora_inicio'),
+			'hora_fin' => date("Y-m-d ").$this->input->post('hora_fin')
+			);
+			$this->horarios_model->editar_horario($data);
+			$var["notificacion"] = "<strong>Éxito!</strong> Viático: '".$this->input->post('descripcion')."' modificado exitosamente.";
+
+		}else if($this->input->post('band') == "delete"){
+
+			$data = array(
+			'idhorario' => $this->input->post('idhorario')
+			);
+			$this->horarios_model->eliminar_horario($data);
+			$var["notificacion"] = "<strong>Éxito!</strong> Viático: '".$this->input->post('descripcion')."' eliminado exitosamente.";
+
+		}
+		$var["horarios"] = $this->horarios_model->mostrar_horario();
+		$this->vista_horario($var);
 	}
 }
 ?>
--- application/views/configuraciones/horarios.php
+++ application/views/configuraciones/horarios.php
@@ -1,10 +1,82 @@
+<script type="text/javascript">
+    function cambiar_editar(id,descripcion,hora_inicio,hora_fin){
+        $("#idhorario").val(id);
+        $("#descripcion").val(descripcion);
+        $("#hora_inicio").val(hora_inicio);
+        $("#hora_fin").val(hora_fin);
+
+        $("#ttl_form").removeClass("bg-success");
+        $("#ttl_form").addClass("bg-info");
+
+        $("#btnadd").hide(0);
+        $("#btnedit").show(0);
+
+        $("#cnt-tabla").hide(0);
+        $("#cnt_form").show(0);
+
+        $("#ttl_form").children("h4").html("<span class='fa fa-wrench'></span> Editar viático");
+    }
+
+    function cambiar_nuevo(){
+        $("#idhorario").val("");
+        $("#descripcion").val("");
+        $("#hora_inicio").val("");
+        $("#hora_fin").val("");
+        $("#band").val("save");
+
+        $("#ttl_form").addClass("bg-success");
+        $("#ttl_form").removeClass("bg-info");
+
+        $("#btnadd").show(0);
+        $("#btnedit").hide(0);
+
+        $("#cnt-tabla").hide(0);
+        $("#cnt_form").show(0);
+
+        $("#ttl_form").children("h4").html("<span class='mdi mdi-plus'></span> Nuevo viático");
+    }
+
+    function pantalla_div(obj){
+        if($(obj).children("i").hasClass("mdi-fullscreen")){        
+            $(obj).parent().parent().parent().parent("div").css({"padding-left":"0px","padding-right":"0px"});
+            $(obj).html("<i class='mdi mdi-fullscreen-exit'></i>");
+        }else{
+            $(obj).parent().parent().parent().parent("div").css({"padding-left":"100px","padding-right":"100px"});
+            $(obj).html("<i class='mdi mdi-fullscreen'></i>");
+        }
+    }
+
+    function cerrar_mantenimiento(){
+        $("#cnt-tabla").show(0);
+        $("#cnt_form").hide(0);
+    }
+
+    function editar_horario(obj){
+        $("#band").val("edit");
+        $(obj).parent().parent().submit();
+    }
+
+    function eliminar_horario(obj){
+        $("#band").val("delete");
+        $(obj).parent().parent().submit();
+    }
+
+
+    function iniciar(){
+        <?php if($notificacion != nada){ ?>
+            
+        <?php } ?>
+    }
+
+</script>
+
 <!-- ============================================================== -->
-<!-- Div de inicio (envoltura) -->
+<!-- Inicio de DIV de inicio (ENVOLTURA) -->
 <!-- ============================================================== -->
 <div class="page-wrapper">
 	<div class="container-fluid">
 		<!-- ============================================================== -->
-		<!-- Título de la página de sección -->
+		<!-- TITULO de la página de sección -->
 		<!-- ============================================================== -->
 	    <div class="row page-titles">
             <div class="align-self-center" align="center">
@@ -12,23 +84,33 @@
             </div>
         </div>
         <!-- ============================================================== -->
-		<!-- Fin Título de la página de sección -->
+		<!-- Fin TITULO de la página de sección -->
 		<!-- ============================================================== -->
 		<!-- ============================================================== -->
-		<!-- Cuerpo de la página de sección -->
+		<!-- Inicio del CUERPO DE LA SECCIÓN -->
 		<!-- ============================================================== -->
 		<div class="row">
-            <div class="col-lg-4">
+            <!-- ============================================================== -->
+            <!-- Inicio del FORMULARIO de gestión -->
+            <!-- ============================================================== -->
+            <div class="col-lg-12" id="cnt_form" style="display: none; padding-left: 100px; padding-right: 100px;">
                 <div class="card">
-                	<div class="card-body bg-success">
-                		<h4 class="text-white card-title" style="margin-bottom: 0px;"><span class="mdi mdi-plus"></span> Nuevo viático</h4>
-                	</div>
-                    <div class="card-body">
-                        <form class="m-t-40" style="margin-top: 0px;">
+                    <div class="card-header bg-success2" id="ttl_form">
+                        <div class="card-actions text-white">
+                            <a style="font-size: 16px;" onclick="pantalla_div(this);"><i class="mdi mdi-fullscreen"></i></a>
+                            <a style="font-size: 16px;" onclick="cerrar_mantenimiento();"><i class="mdi mdi-window-close"></i></a>
+                        </div>
+                        <h4 class="card-title m-b-0 text-white">Listado de viáticos</h4>
+                    </div>
+                    <div class="card-body b-t">
+                        
+                        <?php echo form_open('horarios/gestionar_horarios', array('style' => 'margin-top: 0px;', 'class' => 'm-t-40')); ?>
+                            <input type="hidden" id="band" name="band" value="save">
+                            <input type="hidden" id="idhorario" name="idhorario" value="">
                             <div class="form-group">
                                 <h5>Descripción: <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="text" name="text" class="form-control" required="" placeholder="desayuno, almuerzo, cena" data-validation-required-message="Este campo es requerido">
+                                    <input type="text" id="descripcion" name="descripcion" class="form-control" required="" placeholder="desayuno, almuerzo, cena" data-validation-required-message="Este campo es requerido">
                                     <div class="help-block"></div>
                                 </div>
                             </div>
@@ -36,7 +118,7 @@
                             <div class="form-group">
                                 <h5>Hora inicio: <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="time" name="text" class="form-control" required="" placeholder="desayuno, almuerzo, cena" data-validation-required-message="Este campo es requerido">
+                                    <input type="time" id="hora_inicio" name="hora_inicio" class="form-control" required="" placeholder="desayuno, almuerzo, cena" data-validation-required-message="Este campo es requerido">
                                     <div class="help-block"></div>
                                 </div>
                             </div>
@@ -44,25 +126,41 @@
                             <div class="form-group">
                                 <h5>Hora fin: <span class="text-danger">*</span></h5>
                                 <div class="controls">
-                                    <input type="time" name="text" class="form-control" required="" placeholder="desayuno, almuerzo, cena" data-validation-required-message="Este campo es requerido">
+                                    <input type="time" id="hora_fin" name="hora_fin" class="form-control" required="" placeholder="desayuno, almuerzo, cena" data-validation-required-message="Este campo es requerido">
                                     <div class="help-block"></div>
                                 </div>
                             </div>
-                            <div align="right">
-                            	<button type="button" class="btn waves-effect waves-light btn-success2"><i class="mdi mdi-plus"></i> Guardar</button>
+                            <div align="right" id="btnadd">
+                            	<button type="submit" class="btn waves-effect waves-light btn-success2"><i class="mdi mdi-plus"></i> Guardar</button>
+                            </div>
+                            <div align="right" id="btnedit" style="display: none;">
+                                <button type="button" onclick="editar_horario(this)" class="btn waves-effect waves-light btn-info"><i class="mdi mdi-pencil"></i> Editar</button>
+                                <button type="button" onclick="eliminar_horario(this)" class="btn waves-effect waves-light btn-danger"><i class="mdi mdi-window-close"></i> Eliminar</button>
                             </div>
 
-                        </form>
+                        <?php echo form_close(); ?>
                     </div>
                 </div>
             </div>
-            <div class="col-lg-8">
-
-            	<div class="ribbon-wrapper card">
-                    <div class="ribbon ribbon-default">Tabla de horario de viáticos</div>
-                    <button class="btn ribbon ribbon-corner ribbon-success ribbon-right"><span class="mdi mdi-plus"></span></button>
-                    <div class="ribbon-content">
-                    	<div class="table-responsive" style="margin-top: 0px;">
+            <!-- ============================================================== -->
+            <!-- Fin del FORMULARIO de gestión -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- Inicio de la TABLA -->
+            <!-- ============================================================== -->
+            <div class="col-lg-12" id="cnt-tabla">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-actions">
+                            <a onclick="pantalla_div(this);"><i class="mdi mdi-fullscreen-exit"></i></a>
+                        </div>
+                        <h4 class="card-title m-b-0">Listado de viáticos</h4>
+                    </div>
+                    <div class="card-body b-t">
+                        <div class="pull-right">
+                            <button type="button" onclick="cambiar_nuevo();" class="btn btn-rounded btn-success2"><span class="mdi mdi-plus"></span> Nuevo registro</button>
+                        </div>
+                        <div class="table-responsive" style="margin-top: 0px;">
                             <table id="myTable" class="table table-bordered">
                                 <thead class="bg-info text-white">
                                     <tr>
@@ -70,37 +168,42 @@
                                         <th>Descripción</th>
                                         <th>Inicio</th>
                                         <th>Fin</th>
+                                        <th>(*)</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                    	<td>1</td>
-                                        <td>Desayuno</td>
-                                        <td>8:00 am</td>
-                                        <td>10:00 am</td>
-                                    </tr>
-                                    <tr>
-                                    	<td>2</td>
-                                        <td>Almerzo</td>
-                                        <td>12:00 pm</td>
-                                        <td>01:00 pm</td>
-                                    </tr>
+                                <?php 
+                                    if(!empty($horarios)){
+                                        foreach ($horarios->result() as $fila) {
+                                           echo "<tr>";
+                                           echo "<td>".$fila->id_horario_viatico."</td>";
+                                           echo "<td>".$fila->descripcion."</td>";
+                                           echo "<td>".date("h:i A",strtotime($fila->hora_inicio))."</td>";
+                                           echo "<td>".date("h:i A",strtotime($fila->hora_fin))."</td>";
+                                           
+                                           $array = array($fila->id_horario_viatico, $fila->descripcion, date("H:i",strtotime($fila->hora_inicio)), date("H:i",strtotime($fila->hora_fin)));
+                                           echo boton_tabla($array,"cambiar_editar");
+                                           echo "</tr>";
+                                        }
+                                    }
+                                ?>
                                 </tbody>
                             </table>
                         </div>
                     </div>
                 </div>
-
             </div>
+            <!-- ============================================================== -->
+            <!-- Fin de la TABLA -->
+            <!-- ============================================================== -->
         </div>
         <!-- ============================================================== -->
-		<!-- Fin Cuerpo de la página de sección -->
+		<!-- Fin CUERPO DE LA SECCIÓN -->
 		<!-- ============================================================== -->
-    </div>
-
+    </div> 
 </div>
 <!-- ============================================================== -->
-<!-- Fin de Div de inicio (envoltura) -->
+<!-- Fin de DIV de inicio (ENVOLTURA) -->
 <!-- ============================================================== -->
 
 
