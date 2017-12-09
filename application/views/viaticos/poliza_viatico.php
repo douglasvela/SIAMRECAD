556c3689507778e028964de2fb28ee34ba03f085
elisaalvz <32070700+elisaalvz@users.noreply.github.com>
2017-12-09 00:02:43 -0600

    poliza

--- application/views/viaticos/poliza_viatico.php
+++ application/views/viaticos/poliza_viatico.php
@@ -1,103 +1,110 @@
-<div class="card">
-  <div class="card-header">
-    <div class="card-actions">
-           
-    </div>
-      <h4 align="center" class="card-title m-b-0">MINISTERIO DE TRABAJO Y PREVISION SOCIAL <p align="center">POLIZA DE REINTEGRO DEL FONDO CIRCULANTE</p></h4>
-        
-</div>
-    <div class="card-body b-t"  style="padding-top: 7px;">
-      <div class="table-responsive">
-      <div class="form-group col-lg-12">
-        <div align="center">
+
+     <div class="card">
+  <div class="page-wrapper">
+    <div class="container-fluid">
+        <!-- ============================================================== -->
+        <!-- TITULO de la página de sección -->
+        <!-- ============================================================== -->
+        <div class="row page-titles">
+            <div class="align-self-center" align="center">
+                 <h4 align="center" class="card-title m-b-0">MINISTERIO DE TRABAJO Y PREVISION SOCIAL <p align="center">POLIZA DE REINTEGRO DEL FONDO CIRCULANTE</p></h4>
+   
+       
           <table width="1206" height="166" border="0">
             <tr>
-              <td width="308" height="25"> <h5 align="left">INSTITUCIÓN:</h5></td>
-              <td width="318"><span class="controls">
+              <td> <h5 align="justify">INSTITUCIÓN:</h5></td>
+              <td ><div align="justify"><span class="controls">
                 <input type="text" id="nombre2" name="nombre2" class="form-control" />
-              </span></td>
-              <td width="326"><h5 align="left">No. POLIZA: </h5></td>
-              <td width="257"><span class="controls">
+              </span></div></td>
+              <td width="326"><h5 align="justify">No. POLIZA: </h5></td>
+              <td width="257"><div align="justify"><span class="controls">
                 <input type="text" id="nombre7" name="nombre7" class="form-control"  />
-              </span></td>
+              </span></div></td>
             </tr>
             <tr>
-              <td height="25"><h5 align="left">CÓDIGO PRESUPUESTARIO: </h5></td>
-              <td><span class="controls">
+              <td height="25"><h5 align="justify">CÓDIGO PRESUPUESTARIO: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre3" name="nombre3" class="form-control"/>
-              </span></td>
-              <td> <h5 align="left"> MES:</h5></td>
-              <td><span class="controls">
+              </span></div></td>
+              <td> <h5 align="justify"> MES:</h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre8" name="nombre8" class="form-control" />
-              </span></td>
+              </span></div></td>
             </tr>
             <tr>
-              <td height="25"><h5 align="left">DENOMINACIÓN DEL MONTO FIJO: </h5></td>
-              <td><span class="controls">
+              <td height="25"><h5 align="justify">DENOMINACIÓN DEL MONTO FIJO: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre4" name="nombre4" class="form-control"  />
-              </span></td>
-              <td> <h5 align="left"> EJERCICIO FINANCIERO FISCAL: </h5></td>
-              <td><span class="controls">
+              </span></div></td>
+              <td> <h5 align="justify"> EJERCICIO FINANCIERO FISCAL: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre9" name="nombre9" class="form-control"/>
-              </span></td>
+              </span></div></td>
             </tr>
             <tr>
-              <td height="25"> <h5 align="left"> MONTO TOTAL DEL REINTEGRO: </h5></td>
-              <td><span class="controls">
+              <td height="25"> <h5 align="justify"> MONTO TOTAL DEL REINTEGRO: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre5" name="nombre5" class="form-control" />
-              </span></td>
-              <td> <h5 align="left"> NOMBRE DEL BANCO: </h5></td>
-              <td><span class="controls">
+              </span></div></td>
+              <td> <h5 align="justify"> NOMBRE DEL BANCO: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre10" name="nombre10" class="form-control"/>
-              </span></td>
+              </span></div></td>
             </tr>
             <tr>
-              <td height="25"> <h5 align="left">DENOMINACIÓN DEL MONTO FIJO: </h5></td>
-              <td><span class="controls">
+              <td height="25"> <h5 align="justify">DENOMINACIÓN DEL MONTO FIJO: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre6" name="nombre6" class="form-control" required="" data-validation-required-message="Este campo es requerido" />
-              </span></td>
-              <td> <h5 align="left">No. CUENTA BANCARIA: </h5></td>
-              <td><span class="controls">
+              </span></div></td>
+              <td> <h5 align="justify">No. CUENTA BANCARIA: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre11" name="nombre11" class="form-control" required="required" data-validation-required-message="Este campo es requerido" />
-              </span></td>
+              </span></div></td>
             </tr>
             <tr>
-              <td height="25"> <h5 align="left">CANTIDAD EN LETRAS: </h5></td>
-              <td><span class="controls">
+              <td height="25"> <h5 align="justify">CANTIDAD EN LETRAS: </h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre" name="nombre" class="form-control" required="" data-validation-required-message="Este campo es requerido" />
-              </span></td>
-              <td><h5 align="left">No. COMPROMISO PRESUPUESTARIO:</h5></td>
-              <td><span class="controls">
+              </span></div></td>
+              <td><h5 align="justify">No. COMPROMISO PRESUPUESTARIO:</h5></td>
+              <td><div align="justify"><span class="controls">
                 <input type="text" id="nombre12" name="nombre12" class="form-control" />
-              </span></td>
+              </span></div></td>
             </tr>
-          </table></th >
-        </div>
+          </table>
+    </div>
+       </div>
       </div>
-                                  
-<div class="table-responsive">
- <div class="form-group col-lg-12">
-  <table id="myTable" class="table table-bordered product-overview">
+     
+   
+      
+    <div class="card">
+   
+      <div class="card-body b-t"  style="padding-top: 7px; font-size: 14px;">
+       <div class="table-responsive">
+     
+            <table id="myTable" class="table table-hover product-overview"  >
                 <thead class="bg-info text-white">
+               
                     <tr>
-                        <th width="59"  rowspan="2">No.DOC</th>
-                        <th width="81" rowspan="2">No.POLIZA</th>
-                        <th width="83"  rowspan="2">MES DE LA POLIZA</th>
-                        <th width="189"  rowspan="2">FECHA DE ELABORACION DEL FORMULARIO</th>
-                         <th width="127" rowspan="2">No. CHEQUE O CARGO CTA</th>
-                          <th width="103" rowspan="2">CÓDIGO DEL EMPLEADO</th>
-                          <th width="85"  rowspan="2">FECHA DE MISIÓN</th>
-                          <th width="41" rowspan="2">SEDE</th>
-                          <th width="112"  rowspan="2">CARGO FUNCIONAL</th>
-                          <th width="44" rowspan="2">UP/LT</th>
-                        <th colspan="4" >DETALLE DE OBJETOS ESPECIFICOS </th>
-                        <th width="58"  rowspan="2" style="min-width: 85px;">TOTAL</th>
+                    <th rowspan="2">No.DOC</th>
+                    <th rowspan="2">No.POLIZA</th>
+                    <th rowspan="2">MES DE LA POLIZA</th>
+                    <th rowspan="2">FECHA DE ELABORACION DEL FORMULARIO</th>
+                    <th rowspan="2">No. CHEQUE O CARGO CTA</th>
+                    <th rowspan="2">CÓDIGO DEL EMPLEADO</th>
+                    <th rowspan="2">FECHA DE MISIÓN</th>
+                    <th rowspan="2">SEDE</th>
+                    <th rowspan="2">CARGO FUNCIONAL</th>
+                    <th  rowspan="2">UP/LT</th>
+                   <th colspan="4" ><div align="center">DETALLE DE OBJETOS ESPECIFICOS </div></th>
+                   <th  rowspan="2" >TOTAL</th>
                     </tr>
                     <tr>
-                      <th width="44" >54401</th>
-                      <th width="62" >VALOR</th>
-                      <th width="44" >54403</th>
-                      <th width="62" >VALOR</th>
+                      <th  >54401</th>
+                      <th >VALOR</th>
+                      <th >54403</th>
+                      <th >VALOR</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -105,8 +112,17 @@
                 	
                 ?>
                 </tbody>
-            </table>
-            </div>
+    </table>
+  
+ </div>
+      </div>
+</div>
+</div>
+</div>
+</div>
+
+
+
             <script>
 $(function(){
     $(document).ready(function() {
@@ -114,4 +130,4 @@ $(function(){
     });
 });
             </script>
-</div>
+
