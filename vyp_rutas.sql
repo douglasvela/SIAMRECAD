3e1c670b4015fbdb7f95c6018cd94dfb65b75426
Unknown <d_recinos@outlook.com>
2017-12-11 05:56:17 -0600

    tabla de rutas

--- vyp_rutas.sql
+++ vyp_rutas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 11-12-2017 a las 05:50:27
+-- Tiempo de generación: 11-12-2017 a las 05:56:05
 -- Versión del servidor: 5.7.20-0ubuntu0.17.04.1
 -- Versión de PHP: 7.0.22-0ubuntu0.17.04.1
 
@@ -38,23 +38,6 @@ CREATE TABLE `vyp_rutas` (
   `longitud_destino_vyp_rutas` varchar(200) DEFAULT NULL,
   `opcionruta_vyp_rutas` varchar(35) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Volcado de datos para la tabla `vyp_rutas`
---
-
-INSERT INTO `vyp_rutas` (`id_vyp_rutas`, `id_oficina_origen_vyp_rutas`, `id_oficina_destino_vyp_rutas`, `id_departamento_vyp_rutas`, `id_municipio_vyp_rutas`, `km_vyp_rutas`, `descripcion_destino_vyp_rutas`, `latitud_destino_vyp_rutas`, `longitud_destino_vyp_rutas`, `opcionruta_vyp_rutas`) VALUES
-(22, 1, 2, 00000, 0, 100.00, 'destino oficina', '', '', 'destino_oficina'),
-(23, 1, 0, 00001, 9, 120.00, 'destino municipio', '', '', 'destino_municipio'),
-(24, 3, 0, 00001, 12, 334.00, 'destino mapa', '13.97199560155885', ' -89.75115395151079', 'destino_mapa'),
-(25, 4, 2, 00000, 0, 34.00, 'otro destino oficina', '', '', 'destino_oficina'),
-(26, 1, 0, 00014, 253, 40.00, 'otra ruta mapa', '13.799018213299096', ' -87.89583778940141', 'destino_mapa'),
-(27, 5, 0, 00012, 211, 34.00, 'otro de municipio', '', '', 'destino_municipio'),
-(28, 1, 6, 00000, 0, 4.00, 'dd', '', '', 'destino_oficina'),
-(29, 1, 0, 00012, 208, 3.00, 'ddd', '13.539812599756564', ' -88.27349281869829', 'destino_mapa'),
-(30, 2, 6, 00000, 0, 4.00, 'sdfsdf', '', '', 'destino_oficina'),
-(31, 1, 5, 00000, 0, 32.00, 'sd', '', '', 'destino_oficina'),
-(32, 2, 4, 00000, 0, 23.00, 'dsf', '', '', 'destino_oficina');
 
 --
 -- Índices para tablas volcadas
