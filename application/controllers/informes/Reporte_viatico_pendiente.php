4c2ec291003e6a9c52b7af246a10fc6ad00a51cf
douglasvela <d_recinos@outlook.com>
2018-01-12 09:13:15 -0600

    ATOM 3

--- application/controllers/informes/Reporte_viatico_pendiente.php
+++ application/controllers/informes/Reporte_viatico_pendiente.php
@@ -31,6 +31,7 @@ class Reporte_viatico_pendiente extends CI_Controller {
             array(array('220','20','233'),array('','',''),array('55','22','255')),
             array(array('255','211','0'),array('33','92','19'),array('192','10','2')));
             echo "esto es una prueba desde atom";
+            echo "esto es una prueba desde atom 2";
 
               //Se agrega un salto de linea
             $this->pdf->Ln(5);
