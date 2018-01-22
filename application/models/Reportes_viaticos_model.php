15ea590598b54948708825ac754e2d117d32e91e
douglasvela <d_recinos@outlook.com>
2018-01-22 15:30:47 -0600

    consultas

--- application/models/Reportes_viaticos_model.php
+++ application/models/Reportes_viaticos_model.php
@@ -54,6 +54,10 @@ SELECT mo.id_mision_oficial FROM vyp_mision_oficial AS mo WHERE mo.nr_empleado=2
     }
 }
 /*
+CONSULTA TERMINADA
+SELECT distinct t2.nombre_seccion AS DESCRIPCION, t2.id_seccion,t2.depende FROM org_seccion as t1 LEFT JOIN org_seccion AS t2 ON (t2.depende = t1.id_seccion OR t1.id_seccion = t2.id_seccion ) where t1.depende='40' ORDER BY t1.depende ASC, t1.id_seccion ASC, t2.id_seccion ASC, t2.depende ASC
+
+
 consulta anidada
 SELECT distinct t2.nombre_seccion AS DESCRIPCION, t2.id_seccion
   FROM org_seccion as t1
@@ -69,5 +73,15 @@ SELECT mo.nr_empleado, mo.nombre_completo, SUM(em.pasajes) AS pasajes, SUM(em.vi
   FROM org_seccion as t1
 LEFT JOIN org_seccion AS t2 ON (t2.depende = t1.id_seccion OR t1.id_seccion = t2.id_seccion )
 ORDER BY t1.depende ASC, t1.id_seccion ASC, t2.id_seccion ASC, t2.depende ASC) GROUP BY mo.nr_empleado ORDER BY total DESC
+
+consulta ordenada
+select s1.id_seccion,s2.id_seccion,s3.id_seccion,s4.id_seccion, s5.id_seccion from org_seccion s1 LEFT JOIN org_seccion as s2 ON s2.id_seccion=s1.depende LEFT JOIN org_seccion as s3 ON s3.id_seccion=s2.depende
+LEFT JOIN org_seccion as s4 ON s4.id_seccion=s3.depende
+LEFT JOIN org_seccion as s5 ON s5.id_seccion=s4.depende
+ORDER BY `s5`.`id_seccion`  DESC,
+ `s4`.`id_seccion`  DESC,
+ `s3`.`id_seccion`  DESC,
+ `s2`.`id_seccion`  DESC,
+ `s1`.`id_seccion`  DESC
 */
 ?>
