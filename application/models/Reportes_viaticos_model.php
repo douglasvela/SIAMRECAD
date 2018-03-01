6e400e3a7fc1938ae8ba02260df79a9eb55ded09
douglasvela <d_recinos@outlook.com>
2018-03-01 10:52:55 -0600

    Consulta sql

--- application/models/Reportes_viaticos_model.php
+++ application/models/Reportes_viaticos_model.php
@@ -129,5 +129,15 @@ SELECT mision.nr_empleado,mision.id_mision_oficial,sum(viatico.viatico),empleado
 
 jose.pleitez
 rolando.carrillo
+
+
+SELECT info.id_empleado,info.id_cargo_funcional,info.id_seccion,empleado.nr,CONCAT(empleado.primer_nombre,' ',empleado.segundo_nombre) as nombre,mision.id_mision_oficial,sum(viatico.viatico), seccion.nombre_seccion FROM sir_empleado_informacion_laboral as info
+INNER JOIN sir_empleado as empleado ON empleado.id_empleado=info.id_empleado
+INNER JOIN vyp_mision_oficial as mision ON empleado.nr = mision.nr_empleado
+INNER JOIN vyp_empresa_viatico as viatico ON viatico.id_mision=mision.id_mision_oficial
+INNER JOIN org_seccion as seccion ON seccion.id_seccion=info.id_seccion
+WHERE info.id_cargo_funcional = 291
+GROUP BY mision.id_mision_oficial
+ORDER BY info.fecha_inicio DESC limit 1
 */
 ?>
