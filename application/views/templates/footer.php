a0db5382861091477ed1bd414f2c5d1a62c4d365
willian0795 <sonryjr@gmail.com>
2018-05-11 00:01:59 -0600

    optimizando y reparacion de menú

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -21,50 +21,24 @@
     <script src="<?php echo base_url(); ?>assets/js/custom.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/mask.js"></script>
     <script src="<?php echo base_url(); ?>assets/js/validation.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/footable/js/footable.all.min.js"></script>
     <!-- Image cropper JavaScript -->
     <script src="<?php echo base_url(); ?>assets/plugins/cropper/cropper.js"></script>
-    <!-- icheck -->
-    <script src="<?php echo base_url(); ?>assets/plugins/icheck/icheck.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/icheck/icheck.init.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.js" type="text/javascript"></script>
     <!-- jQuery file upload -->
     <script src="<?php echo base_url(); ?>assets/plugins/dropify/dist/js/dropify.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-switch/bootstrap-switch.min.js"></script>
-    <script>
-    ! function(window, document, $) {
-        "use strict";
-        $("input,select,textarea").not("[type=submit]").jqBootstrapValidation(), $(".skin-square input").iCheck({
-            checkboxClass: "icheckbox_square-green",
-            radioClass: "iradio_square-green"
-        }), $(".touchspin").TouchSpin(), $(".switchBootstrap").bootstrapSwitch();
-    }(window, document, jQuery);
-    </script>
     <script src="<?php echo base_url(); ?>assets/plugins/moment/min/moment.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/wizard/jquery.steps.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/wizard/jquery.validate.min.js"></script>
     <!-- ============================================================== -->
     <!-- This page plugins -->
     <!-- ============================================================== -->
-    <!-- chartist chart -->
-    <script src="<?php echo base_url(); ?>assets/plugins/chartist-js/dist/chartist.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.min.js"></script>
     <!-- ============================================================== -->
     <!-- This page plugins -->
     <!-- ============================================================== -->
     <script src="<?php echo base_url(); ?>assets/plugins/switchery/dist/switchery.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/select2/dist/js/select2.full.min.js" type="text/javascript"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-select/bootstrap-select.min.js" type="text/javascript"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>assets/plugins/multiselect/js/jquery.multi-select.js"></script>
-    <!--c3 JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/plugins/d3/d3.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/c3-master/c3.min.js"></script>
     <!-- ============================================================== -->
     <!-- Style switcher -->
     <!-- ============================================================== -->
-    <script src="<?php echo base_url(); ?>assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/wizard/steps.js"></script>
     <!-- Sweet-Alert  -->
     <script src="<?php echo base_url(); ?>assets/plugins/sweetalert/sweetalert.min.js"></script>
     <!-- This is data table -->
@@ -80,14 +54,6 @@
     <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-material-datetimepicker/js/bootstrap-material-datetimepicker.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/timepicker/bootstrap-timepicker.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
-    <!-- Clock Plugin JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/plugins/clockpicker/dist/jquery-clockpicker.min.js"></script>
-    <!-- Dropzone Plugin JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/plugins/dropzone-master/dist/dropzone.js"></script>
-    <!-- Color Picker Plugin JavaScript -->
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery-asColorPicker-master/libs/jquery-asColor.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery-asColorPicker-master/libs/jquery-asGradient.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/jquery-asColorPicker-master/dist/jquery-asColorPicker.min.js"></script>
     <!-- Date Picker Plugin JavaScript -->
     <script src="<?php echo base_url(); ?>assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
     <script src="<?php echo base_url(); ?>assets/plugins/toast-master/js/jquery.toast.js"></script>
@@ -139,10 +105,7 @@
             $(".click2edit").summernote('destroy');
         }
     </script>
-    
 
-    <script src="<?php echo base_url(); ?>assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/plugins/vectormap/jquery-jvectormap-us-aea-en.js"></script>
  
      <!-- start - This is for export functionality only -->
     <script src="https://cdn.datatables.net/buttons/1.2.2/js/dataTables.buttons.min.js"></script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -37,37 +37,23 @@
     <link href="<?php echo base_url(); ?>assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/plugins/dropify/dist/css/dropify.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/plugins/Magnific-Popup-master/dist/magnific-popup.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/plugins/wizard/steps.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/plugins/sweetalert/sweetalert.css" rel="stylesheet" type="text/css">
     <link href="<?php echo base_url(); ?>assets/plugins/toast-master/css/jquery.toast.css" rel="stylesheet">
     <!-- Cropper CSS -->
     <link href="<?php echo base_url(); ?>assets/plugins/cropper/cropper.min.css" rel="stylesheet">
     <!-- chartist CSS -->
-    <link href="<?php echo base_url(); ?>assets/plugins/chartist-js/dist/chartist.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/plugins/chartist-js/dist/chartist-init.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/plugins/chartist-plugin-tooltip-master/dist/chartist-plugin-tooltip.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/plugins/footable/css/footable.core.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/plugins/bootstrap-material-datetimepicker/css/bootstrap-material-datetimepicker.css" rel="stylesheet">
     <!-- Page plugins css -->
-    <link href="<?php echo base_url(); ?>assets/plugins/clockpicker/dist/jquery-clockpicker.min.css" rel="stylesheet">
-    <!-- Color picker plugins css -->
-    <link href="<?php echo base_url(); ?>assets/plugins/jquery-asColorPicker-master/css/asColorPicker.css" rel="stylesheet">
     <!-- Date picker plugins css -->
     <link href="<?php echo base_url(); ?>assets/plugins/bootstrap-datepicker/bootstrap-datepicker.min.css" rel="stylesheet" type="text/css" />
     <!-- Select plugins css -->
     <link href="<?php echo base_url(); ?>assets/plugins/select2/dist/css/select2.min.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url(); ?>assets/plugins/switchery/dist/switchery.min.css" rel="stylesheet" />
     <link href="<?php echo base_url(); ?>assets/plugins/bootstrap-select/bootstrap-select.min.css" rel="stylesheet" />
-    <link href="<?php echo base_url(); ?>assets/plugins/bootstrap-tagsinput/dist/bootstrap-tagsinput.css" rel="stylesheet" />
-    <link href="<?php echo base_url(); ?>assets/plugins/bootstrap-touchspin/dist/jquery.bootstrap-touchspin.min.css" rel="stylesheet" />
-    <link href="<?php echo base_url(); ?>assets/plugins/multiselect/css/multi-select.css" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url(); ?>assets/plugins/icheck/skins/all.css" rel="stylesheet">
     <!-- Daterange picker plugins css -->
     <link href="<?php echo base_url(); ?>assets/plugins/timepicker/bootstrap-timepicker.min.css" rel="stylesheet">
     <link href="<?php echo base_url(); ?>assets/plugins/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
     <!--This page css - Morris CSS -->
-    <link href="<?php echo base_url(); ?>assets/plugins/c3-master/c3.min.css" rel="stylesheet">
-    <link href="<?php echo base_url(); ?>assets/plugins/dropzone-master/dist/dropzone.css" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url(); ?>assets/plugins/summernote/dist/summernote.css" rel="stylesheet" />
     <!-- Custom CSS -->
     <link href="<?php echo base_url(); ?>assets/css/style.css" rel="stylesheet">
