aa51cba6c32fa6f3b3442f57b721fcc6bccb9906
douglas <d_recinos@outlook.com>
2018-12-19 16:46:12 -0600

    config app

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -19,7 +19,10 @@ if(floatval($ua['version']) < $this->config->item("last_version")){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><meta name="description" content=""><meta name="author" content="">
+    <meta charset="utf-8"><meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content=""><meta name="author" content="">
+    <meta name="mobile-web-app-capable" content="yes">
     <link rel="icon" type="image/png" sizes="16x16" href="<?php echo base_url(); ?>assets/images/Logo-min.png">
     <title>SIVIC</title>
     <!-- CSS Requerido -->
